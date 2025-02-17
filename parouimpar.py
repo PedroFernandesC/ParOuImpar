9b5559f2a6c3da7bc22e7a130894d255f0b9cd95
João Pedro Fernandes Cabral <156030700+PedroFernandesC@users.noreply.github.com>
2025-02-16 21:29:34 -0300

    Primeiro commit

---
+++ parouimpar.py
@@ -0,0 +1,8 @@
+# Verificador de Par ou Ímpar
+numero = int(input("Digite um número para verificar se ele é par ou ímpar: "))
+verificador = numero % 2
+
+if verificador == 0:
+    print(f"O número {numero} é par!")
+else:
+    print(f"O número {numero } é ímpar!")
